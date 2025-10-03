785c7930ef7b2099faadcb5721b03204d328a060
Matheus Mattos <matheus@3ainvestimentos.com.br>
2025-10-03 20:33:40 +0000

    Altere a fonte para Roboto (Peso 700 - Negrito) (_for element h1_)

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -23,7 +23,7 @@ export function PageHeader() {
   return (
     <header className="sticky top-0 z-10 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
       <SidebarTrigger className="md:hidden" />
-      <h1 className="font-headline text-2xl font-semibold">
+      <h1 className="font-headline text-2xl font-bold">
         {getTitle(pathname)}
       </h1>
     </header>
