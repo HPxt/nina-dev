5332ad481f08c7b0dc3225781bcf7cf9d5038ea4
Matheus Mattos <matheus@3ainvestimentos.com.br>
2025-10-06 15:16:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9152494730-25d31",
+  "appId": "1:148437696289:web:c65f336161a04b937c4860",
+  "apiKey": "AIzaSyDC1C2dpFWPPsGew2XPVSNIKZ3J6DJHIp4",
+  "authDomain": "studio-9152494730-25d31.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "148437696289"
+};
