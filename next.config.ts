dc5e1a85ac0b150aa3a3c01ae3d3aa0522d55a3e
Matheus Mattos <matheus@3ainvestimentos.com.br>
2025-10-16 19:58:59 +0000

    Try fixing this error: `Runtime Error: Error: ./README.md Unknown module

--- next.config.ts
+++ next.config.ts
@@ -37,13 +37,8 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.md$/,
-      use: 'raw-loader',
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
+
+    
