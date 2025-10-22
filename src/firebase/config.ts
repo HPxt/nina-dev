383864eca5375e266d185ad6d4a0fcc99032ac7d
Henrique Peixoto <hppeixoto14@gmail.com>
2025-10-22 14:46:15 -0300

    tirando api key

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,9 @@
+import { config } from 'dotenv';
+config();
 export const firebaseConfig = {
   "projectId": "studio-9152494730-25d31",
   "appId": "1:148437696289:web:c65f336161a04b937c4860",
-  "apiKey": "AIzaSyDC1C2dpFWPPsGew2XPVSNIKZ3J6DJHIp4",
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   "authDomain": "studio-9152494730-25d31.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "148437696289"
